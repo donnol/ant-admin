2883d686dfbf490497b8e286edb24dc7bf20cd2d
jdlau <jdlau@qq.com>
2019-08-04 15:46:14 +0800

    修改方法名

--- src/models/note.js
+++ src/models/note.js
@@ -12,28 +12,28 @@ export default {
       });
     },
     async get({ payload }, { dispatch }) {
-      return await request("/note/get", {
+      return await request("/note", {
         method: "GET",
         query: payload,
         autoCheck: true
       });
     },
     async del({ payload }, { dispatch }) {
-      return await request("/note/del", {
+      return await request("/note", {
         method: "DELETE",
         body: payload,
         autoCheck: true
       });
     },
     async mod({ payload }, { dispatch }) {
-      return await request("/note/mod", {
+      return await request("/note", {
         method: "PUT",
         body: payload,
         autoCheck: true
       });
     },
     async add({ payload }, { dispatch }) {
-      return await request("/note/add", {
+      return await request("/note", {
         method: "POST",
         body: payload,
         autoCheck: true
