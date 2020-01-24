cc6b3976d00ab4770472838239232141bdf5d1a0
jdlau <jdlau@qq.com>
2020-01-24 16:58:16 +0800

    添加state和切换按钮

--- src/pages/Front/Note/App.js
+++ src/pages/Front/Note/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import style from "./App.less";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    console.log("constructor");
+    this.state = { show: false };
+  }
+  click = () => {
+    console.log("click", this.state.show);
+    this.setState({ show: !this.state.show });
+    console.log("click", this.state.show);
+  };
   render = () => {
-    let show = <div>show</div>;
-    let left = function(name) {
+    let show = this.state.show;
+    let left = function(name, i) {
       return (
-        <div className={style.AppLeft}>
+        <div key={i} className={style.AppLeft}>
           <div className={style.Border}>{name}</div>
         </div>
       );
@@ -22,13 +32,20 @@ export default class App extends React.Component {
     return (
       <div>
         {/* 条件 */}
-        {show ? show : !show}
+        {show ? "a" : "b"}
+        <div>
+          <button onClick={this.click}>Click</button>
+        </div>
         {/* 循环 */}
         {users.map((user, i) =>
           // 需要记住一个简单的规则：对于用表达式套数组罗列到页面上的元素，都要为每个元素加上 key 属性，这个 key 必须是每个元素唯一的标识。一般来说，key 的值可以直接后台数据返回的 id，因为后台的 id 都是唯一的。
           // 有时候id也不是唯一的，还要加上类型等其它额外字段
           // 如果表达式又放到了html元素里，也要加上大括号，没有则可以直接使用
-          i % 2 === 0 ? left(user.name) : <div key={i}>{right(user.name)}</div>
+          i % 2 === 0 ? (
+            left(user.name, i)
+          ) : (
+            <div key={i}>{right(user.name)}</div>
+          )
         )}
       </div>
     );
